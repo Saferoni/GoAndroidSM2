b5d5bac4188a62ad0897d2704e9a325082edd7ad
Saferoni <SafronovPN@gmail.com>
2016-12-25 19:22:55 +0200

    Дополнительный клас

---
+++ app/src/main/java/com/safercrypt/goandroidsm2/Activity2.java
@@ -0,0 +1,34 @@
+package com.safercrypt.goandroidsm2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // пример работы с ListView
+        // получаем экземпляр элемента ListView
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+// определяем массив типа String
+        final String[] catNames = new String[] {
+                "Рыжик", "Барсик", "Мурзик", "Мурка", "Васька",
+                "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+                "Китти", "Масяня", "Симба"
+        };
+
+// используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, catNames);
+
+        listView.setAdapter(adapter);
+    }
+}
